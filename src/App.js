5b683eaa57a3e2303f580c9a35076d249bed38bc
nbroeder <nbroeder@uwo.ca>
2021-11-20 10:30:13 -0500

    chatbot component made

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css'
+import  chatbotShell  from './Components/chatbotShell.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+       <chatbotShell></chatbotShell>
+        
       </header>
     </div>
   );
